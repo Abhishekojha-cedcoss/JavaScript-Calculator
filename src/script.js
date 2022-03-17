7146fe72ee3bb270eccb97b684d51107895147c9
Abhishek <abhishekojha@cedcoss.com>
2022-03-17 09:58:55 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,84 @@
+var c = 0,
+  oldvalue,
+  check,
+  operatorvalue,
+  v,
+  call = 0;
+function input(n) {
+  if (call == 0) {
+    document.getElementById("result").value += n;
+    v = document.getElementById("result").value;
+  } else {
+    document.getElementById("result").value = "";
+    document.getElementById("result").value = n;
+    v = document.getElementById("result").value;
+    call = 0;
+  }
+}
+function calc(o, op, newvalue) {
+  o = parseInt(o);
+  newvalue = parseInt(newvalue);
+  switch (op) {
+    case "+":
+      document.getElementById("result").value = o + newvalue;
+      break;
+    case "-":
+      document.getElementById("result").value = o - newvalue;
+      break;
+    case "*":
+      document.getElementById("result").value = o * newvalue;
+      break;
+    case "/":
+      document.getElementById("result").value = o / newvalue;
+      break;
+  }
+}
+
+function equalto() {
+  oldvalue = parseInt(oldvalue);
+  v = parseInt(v);
+  switch (operatorvalue) {
+    case "+":
+      document.getElementById("result").value = oldvalue + v;
+      break;
+    case "-":
+      document.getElementById("result").value = oldvalue - v;
+      break;
+    case "*":
+      document.getElementById("result").value = oldvalue * v;
+      break;
+    case "/":
+      document.getElementById("result").value = oldvalue / v;
+      break;
+  }
+}
+function operatorcheck(check) {
+  c = c + 1;
+  call = 1;
+  if (c == 2) {
+    calc(oldvalue, operatorvalue, v);
+    c = 1;
+    operatorvalue = check;
+    oldvalue = document.getElementById("result").value;
+  } else {
+    oldvalue = document.getElementById("result").value;
+
+    switch (check) {
+      case "+":
+        operatorvalue = "+";
+        break;
+      case "-":
+        operatorvalue = "-";
+        break;
+      case "*":
+        operatorvalue = "*";
+        break;
+      case "/":
+        operatorvalue = "/";
+        break;
+    }
+  }
+}
+function clr() {
+  window.location.href = "index.html";
+}
